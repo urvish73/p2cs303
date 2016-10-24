2f1a45184225d3fbaca2a54fedff7c4859adf5bd
Urvish <ujs8t2@mail.umkc.edu>
2016-10-24 10:11:42 -0500

    Add files via upload
    Book class is for sure not completely done. I still need to figure out a way to work on the number of employees (using queue) who receive the book!!

---
+++ Book.cpp
@@ -0,0 +1,39 @@
+#include "Book.h"
+
+book::book(const book& var)
+{
+    book_name = var.book_name;
+    startDate = var.startDate;
+    endDate = var.endDate;
+    archive = var.archive;
+}
+
+string book::get_name()
+{
+    return book_name;
+}
+
+Date book::get_beginning_date()
+{
+    return startDate;
+}
+
+Date book::get_ending_date()
+{
+    return endDate;
+}
+
+void book::set_name(string m_name)
+{
+    book_name = m_name;
+}
+
+void book::set_beginning_date(Date m_startDate)
+{
+    startDate = m_startDate;
+}
+
+void book::set_ending_date(Date m_endDate)
+{
+    endDate = m_endDate;
+}
---
+++ Book.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <queue>
+#include "Date.h"
+
+using namespace std;
+
+class book
+{
+public:
+    book() {};
+    book(const book& var);
+
+    string get_name();
+    void set_name(string m_name);
+
+    Date get_beginning_date();
+    void set_beginning_date(Date m_startDate);
+    Date get_ending_date();
+    void set_ending_date(Date m_endDate);
+
+private:
+    string book_name;
+    Date startDate;
+    Date endDate;
+    bool archive;
+};
