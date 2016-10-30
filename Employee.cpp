43bfc2a57531a35b509c264c8a00a32187288f66
Nathaniel A. Barnett <thegrtn@gmail.com>
2016-10-30 12:21:21 -0500

    Employee class and header
    I have not written the body for the methods "set_retaining_time()" and "set_waiting_time()" but, I am trying to figure out what to pass into those methods so that I can grab the needed info.

---
+++ Employee.cpp
@@ -0,0 +1,16 @@
+/*
+Name: Nathaniel Barnett
+cpp file for body functions of Employee class.
+*/
+
+#include "Employee.h"
+
+void Employee::set_retaining_time()
+{
+
+}
+
+void Employee::set_waiting_time()
+{
+
+}
