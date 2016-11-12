2353e3c5ff9962841054373501a1938adc4ee953
License2Learn <ambd5d@mail.umkc.edu>
2016-11-11 20:06:17 -0600

    Update Book.h

--- Book.h
+++ Book.h
@@ -1,6 +1,3 @@
-//Urvish Shah
-//Book class
-
 #ifndef Book_H
 #define Book_H
 
@@ -11,36 +8,40 @@
 #include "Date.h"
 #include "Employee.h"
 #include "Priority_Queue.h"
-
 using namespace std;
-//UPDATE
+
 class book {
 public:
-	book();
-	book(string m_name);
-	//book(const book& var);
+    book() { archive = true; }
 
-	string get_name();
-	void set_name(string m_name);
+    bool get_archive();
+    void set_archive(bool arch);
+    
+    string get_name();
+    void set_name(string m_name);
 
-	Date get_beginning_date();
-	void set_beginning_date(Date m_startDate);
-	Date get_ending_date();
-	void set_ending_date(Date m_endDate);
-	Date get_last_circ_date();
-	void set_last_circ_date(Date new_circ_date);
+    Date get_beginning_date();
+    void set_beginning_date(Date m_startDate);
 
-	void fill_employees(list<Employee>& employeeList);
-	void pass_book(Date pass_date);
+    Date get_ending_date();
+    void set_ending_date(Date m_endDate);
+
+    Date get_last_circ_date();
+    void set_last_circ_date(Date new_circ_date);
+
+    void fill_employees(list<Employee>& employeeList);//Fills the books emp_queue with employees fromthe libraries employeeList.
+
+    void pass_book(Date pass_date);
+
+    int get_queueSize();
 
 private:
-	string book_name;
-	Date last_circ_date;
-	Date startDate;
-	Date endDate;
-	bool archive;
+    string book_name;
+    Date last_circ_date;
+    Date startDate;
+    Date endDate;
+    bool archive;
 
-	Priority_Queue emp_queue;
+    Priority_Queue emp_queue;
 };
-
-#endif // !1
+#endif
